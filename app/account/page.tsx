4916bdab9859da3071afa422fc9c7029e3c7859a
mabrig1 <140073061+mabrig1@users.noreply.github.com>
2026-09-15 09:43:47 +0100

    feat: add persistent accounts and MongoDB foundation

---
+++ app/account/page.tsx
@@ -0,0 +1,9 @@
+import AccountPanel from "@/components/account-panel";
+
+export default function AccountPage() {
+  return (
+    <main className="commercialPage">
+      <AccountPanel />
+    </main>
+  );
+}
